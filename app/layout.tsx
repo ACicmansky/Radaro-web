acc536188fdcb1965e5eadbeb53ccba982082a1e
ACicmansky <andrej.cicmansky@gmail.com>
2025-04-05 23:41:14 +0200

    feat: add SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Toaster } from "@/components/ui/sonner"
 import localFont from "next/font/local";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -15,12 +16,22 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "RADARO - Profesionálne stavebné služby",
+  metadataBase: new URL('https://radaro.sk'),
+  title: {
+    default: "RADARO - Profesionálne stavebné služby",
+    template: "%s | RADARO"
+  },
   description: "Profesionálne stavebné služby s dôrazom na kvalitu, efektivitu a inovácie. Stavebný dozor, technické poradenstvo a riadenie projektov.",
-  icons: {
-    icon: "/images/other/favicon.ico",
-    shortcut: "/images/other/favicon.ico",
-    apple: "/images/other/favicon.ico",
+  generator: "Next.js",
+  applicationName: "RADARO",
+  referrer: "origin-when-cross-origin",
+  authors: [{ name: "RADARO", url: "https://radaro.sk" }],
+  creator: "RADARO",
+  publisher: "RADARO",
+  formatDetection: {
+    email: true,
+    address: true,
+    telephone: true,
   },
   keywords: [
     "profesionálne stavebné služby",
@@ -35,6 +46,61 @@ export const metadata: Metadata = {
     "stavebné poradenstvo služby",
     "stavebné riadenie služby",
   ],
+  category: "construction",
+  openGraph: {
+    type: "website",
+    locale: "sk_SK",
+    url: "https://radaro.sk",
+    title: "RADARO - Profesionálne stavebné služby",
+    description: "Profesionálne stavebné služby s dôrazom na kvalitu, efektivitu a inovácie. Stavebný dozor, technické poradenstvo a riadenie projektov.",
+    siteName: "RADARO",
+    images: [
+      {
+        url: "/images/other/og-image.jpg",
+        width: 800,
+        height: 420,
+        alt: "RADARO - Profesionálne stavebné služby",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "RADARO - Profesionálne stavebné služby",
+    description: "Profesionálne stavebné služby s dôrazom na kvalitu, efektivitu a inovácie. Stavebný dozor, technické poradenstvo a riadenie projektov.",
+    images: ["/images/other/og-image.jpg"],
+    creator: "@radaro_sk",
+  },
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 5,
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  icons: {
+    icon: "/images/other/favicon.ico",
+    shortcut: "/images/other/favicon.ico",
+    apple: "/images/other/favicon.ico",
+  },
+  verification: {
+    google: "verification_token", //TODO
+    yandex: "verification_token", //TODO
+  },
+  alternates: {
+    canonical: "https://radaro.sk",
+    languages: {
+      'sk-SK': "https://radaro.sk",
+    },
+  },
 };
 
 export default function RootLayout({
@@ -43,7 +109,57 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="sk">
+      <head>
+        <Script
+          id="local-business-schema"
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "LocalBusiness",
+              "name": "RADARO",
+              "image": "https://radaro.sk/images/other/Logo.png",
+              "@id": "https://radaro.sk",
+              "url": "https://radaro.sk",
+              "telephone": "+421902851275",
+              "address": {
+                "@type": "PostalAddress",
+                "streetAddress": "Petra Víťazoslava Rovnianka 5136/9",
+                "addressLocality": "Martin",
+                "postalCode": "03601",
+                "addressCountry": "SK"
+              },
+              "geo": {
+                "@type": "GeoCoordinates",
+                "latitude": 49.09687,
+                "longitude": 18.91743
+              },
+              "openingHoursSpecification": [
+                {
+                  "@type": "OpeningHoursSpecification",
+                  "dayOfWeek": [
+                    "Monday",
+                    "Tuesday",
+                    "Wednesday",
+                    "Thursday",
+                    "Friday"
+                  ],
+                  "opens": "09:00",
+                  "closes": "17:00"
+                }
+              ],
+              "sameAs": [
+                // "https://www.facebook.com/radaro", TODO
+                // "https://www.instagram.com/radaro"
+              ],
+              "priceRange": "€€",
+              "servesCuisine": "Construction Services",
+              "description": "Profesionálne stavebné služby s dôrazom na kvalitu, efektivitu a inovácie. Stavebný dozor, technické poradenstvo a riadenie projektov."
+            })
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
